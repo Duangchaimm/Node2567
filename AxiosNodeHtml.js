12b40523b76cd93ca9eb3f51f9600869ec98c4d5
Your Name <you@example.com>
2024-02-21 09:28:24 +0700

    .

---
+++ AxiosNodeHtml.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+var bodyParser = require("body-parser");
+
+//const base_url = "https://api.example.com";
+const base_url = "https://localhost:3000";
+
